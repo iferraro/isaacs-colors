b681d2e4a13034447b350eb2d936f7888a59eef6
Isaac Ferraro <afronerd01@outlook.com>
2022-04-22 12:51:57 -0700

    Small Tailwind conversion from standard svg syntax

--- src/components/ColorBar.tsx
+++ src/components/ColorBar.tsx
@@ -6,26 +6,25 @@ interface ColorBarProps {
 }
 
 const ColorBar = ({ colorInfo }: ColorBarProps) => {
-  const [open, setOpen] = useState(false);
+  const [revealed, setRevealed] = useState(false);
 
   const handleClick = () => {
-    setOpen(!open);
+    setRevealed(!revealed);
   };
 
   return (
     <div className="flex m-2">
       <div onClick={handleClick}>
         <svg
-          width={open ? 360 : 840}
-          height={open ? 360 : 120}
-          viewBox={open ? "0 0 1 1" : "0 0 7 1"}
+          viewBox={revealed ? "0 0 1 1" : "0 0 7 1"}
           fill={colorInfo.hex}
+          className={revealed ? "w-[360px] h-[360px]" : "w-[840px] h-[120px]"}
         >
           <path d="M 0,0 v 1 h 7 v -1 z" />
         </svg>
       </div>
-      {open && (
-        <p className="ml-4 text-white font-bold">
+      {revealed && (
+        <p className="ml-4 text-left text-white font-bold">
           {colorInfo.name}
           <br />
           {colorInfo.hex}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      transitionTimingFunction: {
+        "in-expo": "cubic-bezier(0.98,-0.36,0.43,1.27)",
+        "out-expo": "cubic-bezier(0.98,-0.36,0.43,1.27)",
+      },
+    },
   },
   plugins: [],
 };
