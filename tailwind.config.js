aa275e2ac930c4141a38cd5fbaef08de9e59c370
Isaac Ferraro <afronerd01@outlook.com>
2022-05-17 14:56:17 -0700

    Fade-in/out without Headless UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,25 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        theme: ['"Computer Modern"', "Times New Roman"],
+      },
+      keyframes: {
+        "custom-fade": {
+          "0%": {
+            opacity: "0"
+          },
+          "100%": {
+            opacity: "100"
+          }
+        }
+      },
+      animation: {
+        "custom-fade": "custom-fade"
+      },
       transitionTimingFunction: {
         "in-expo": "cubic-bezier(.41,.33,.55,.91)",
         "out-expo": "cubic-bezier(.41,.33,.55,.91)",
-      },
-      fontFamily: {
-        theme: ['"Computer Modern"', "Times New Roman"],
       },
     },
   },
