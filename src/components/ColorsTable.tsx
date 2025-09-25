45e628ca96ff9f82f5d27c61be29e35ae196a378
Isaac Ferraro <ieferraro@outlook.com>
2025-09-25 00:27:53 -0700

    Solved initial load issue

--- src/components/ColorsTable.tsx
+++ src/components/ColorsTable.tsx
@@ -1,5 +1,5 @@
 import type { Color } from "../types.ts";
-import { For, createSignal, Show } from "solid-js";
+import { For, createSignal, createEffect, Show } from "solid-js";
 
 type ColorsTableProps = {
   colors: Color[];
@@ -7,12 +7,17 @@ type ColorsTableProps = {
 
 // TODO:
 // 1. Outline the selected color (likely white)
-// 2. No title in info bar upon page load (start with first color)
 export function ColorsTable(props: ColorsTableProps) {
-  const [selectedColor, setSelectedColor] = createSignal<Color | null>(props.colors[0]);
+  const [selectedColor, setSelectedColor] = createSignal<Color | null>(null);
   const [copiedColor, setCopiedColor] = createSignal<Color | null>(null);
 
-  const copyToClipboard = async (hex: string) => {
+  createEffect(() => {
+    if (!selectedColor() && props.colors.length > 0) {
+      setSelectedColor(props.colors[0]);
+    }
+  });
+
+  async function copyToClipboard(hex: string) {
     try {
       await navigator.clipboard.writeText(hex);
       setCopiedColor(selectedColor());
@@ -20,35 +25,22 @@ export function ColorsTable(props: ColorsTableProps) {
     } catch (err) {
       console.error("Failed to copy to clipboard:", err);
     }
-  };
-
-  const colorChart = (
-    <For each={props.colors}>
-      {(color) => (
-        <div
-          class="w-12 h-12 rounded-md"
-          style={{ "background-color": color.id }}
-          onClick={() => setSelectedColor(color)}
-        ></div>
-      )}
-    </For>
-  );
+  }
 
   const infoBar = (
-    <Show
-      when={selectedColor()}
-      fallback={<div class="text-xl text-white font-black text-gray-700">The Colors</div>}
-    >
-      <div class="pt-4 grid grid-cols-[5rem_1fr_5rem] gap-2">
+    <Show when={selectedColor()}>
+      <div class="grid grid-cols-[2fr_5fr_2fr] gap-2">
         <div
-          class="w-12 h-12 justify-self-center self-center rounded-full"
+          class="w-full h-full justify-self-center self-center rounded-full"
           style={{ "background-color": selectedColor()?.id }}
         ></div>
-        <h1 class="text-lg text-center text-white font-black">{selectedColor()?.name}</h1>
+        <h1 class="text-lg text-center text-white font-black">
+          {selectedColor()?.name ?? "The Colors"}
+        </h1>
         <button
           type="button"
           onClick={() => copyToClipboard(selectedColor()?.id || "")}
-          class="px-3 py-1 text-white font-bold text-sm rounded border-2 border-white hover:bg-blue-600"
+          class="px-3 py-1 text-white font-bold text-sm rounded-full border-2 border-white hover:bg-blue-600"
         >
           {copiedColor() === selectedColor() ? "Copied!" : selectedColor()?.id}
         </button>
@@ -56,11 +48,24 @@ export function ColorsTable(props: ColorsTableProps) {
     </Show>
   );
 
+  const colorChart = (
+    <For each={props.colors}>
+      {(color) => (
+        <button
+          type="button"
+          class="aspect-square rounded-full"
+          style={{ "background-color": color.id }}
+          onClick={() => setSelectedColor(color)}
+        ></button>
+      )}
+    </For>
+  );
+
   return (
-    <div class="flex flex-col gap-2 max-w-md">
+    <div class="flex flex-col gap-2 w-full max-w-md">
       {infoBar}
       <hr />
-      <div class="flex flex-wrap gap-2 justify-center items-center">
+      <div class="grid grid-cols-4 gap-2">
         {colorChart}
       </div>
     </div>
